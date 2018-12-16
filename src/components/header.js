7b8bd8c3ad957398c9aa2772f3db0efe6775c690
Zoe <zoe.richards@springload.co.nz>
2018-12-17 10:31:10 +1300

    cherry-pick add header styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,33 @@
 import React from "react";
 import headerStyles from "./header-css-modules.scss";
 
-export default ({ children }) => (
-  <div className={headerStyles.container}>
-    {children}
-    <nav className="home__navigation" className={headerStyles.bluebackground}>
-      <ul className="nav flexItem flexStart">
+export default () => (
+  // <div className={headerStyles.container}>
+
+    <nav className={`${headerStyles.header__navigation}`}>
+
+      <div
+        className={`${headerStyles.header__item}  ${headerStyles.flex__item} `}
+      >
+        <img
+        className={`${headerStyles.header__img}`}
+        src={icon} alt="icon"
+        />
+        <span className={`${headerStyles.header__grow}`}>GROW </span>
+        <a
+          className={`${headerStyles.header__item}`}
+          href="/home-css-modules"
+        >
+          Springload
+        </a>
+      </div>
+      <div className={headerStyles.logo} />
+      <ul
+        className={`${headerStyles.header__list} ${
+          headerStyles.flex__container
+        } ${headerStyles.flexEnd}`}
+      >
+>>>>>>> b864015... add header styling
         <li>
           <a href="http://www.cs.virginia.edu/~robins/YouAndYourResearch.html">
             Springload
