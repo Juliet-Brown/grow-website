bf941681ae11d8667c4f9bec1baae0a792787927
Zoe <zoe.richards@springload.co.nz>
2018-12-17 14:54:10 +1300

    cherry-pick footer styling

---
+++ src/components/footer.js
@@ -0,0 +1,39 @@
+import React from "react";
+import footerStyles from "./footer.module.scss";
+
+export default ({ footerDescription }) => (
+  <footer
+    className={`${footerStyles.flex__container} ${footerStyles.flexCenter} ${
+      footerStyles.footer
+    }`}
+  >
+    <main
+      className={`${footerStyles.flexItem} ${footerStyles.footer__content} ${
+        footerStyles.flex__container
+      }
+  ${footerStyles.flexCenter}`}
+    >
+      <div className={` ${footerStyles.footer__contentLeft}`}>
+        <h3>Making Tech Better Since 2019</h3>
+        <p>
+          What? We're not at all alike! Some days you get the bear, and some
+          days the bear gets you. Maybe if we felt any human loss as keenly as
+          we feel one of those close to us, human history would be far less
+          bloody. Yesterday I did not know how to eat gagh.
+        </p>{" "}
+      </div>
+      <div className={` ${footerStyles.footer__contentRight}`}>
+        <h3>Space for social media icons.  </h3>
+        <p>
+          {" "}
+          What? We're not at all alike!
+          Some days you get the bear, and some days the bear gets you.{" "}
+        </p>
+      </div>
+    </main>
+    <div className={`${footerStyles.footer__copy}`}>
+      {" "}
+      &copy; {footerDescription}{" "}
+    </div>
+  </footer>
+);
