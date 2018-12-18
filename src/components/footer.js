e9d3cee5e20a188e785a861b68473487988673e0
Zoe <zoe.richards@springload.co.nz>
2018-12-18 14:09:42 +1300

    add and resize icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,6 +3,7 @@ import footerStyles from "./footer.module.scss";
 import facebook from "../../static/facebook_icon.svg";
 import instagram from "../../static/instagram_icon.svg";
 import twitter from "../../static/twitter_icon.svg";
+import github from "../../static/github_icon.svg";
 
 export default ({ footerDescription }) => (
   <footer
@@ -25,11 +26,12 @@ export default ({ footerDescription }) => (
           bloody. Yesterday I did not know how to eat gagh.
         </p>{" "}
       </div>
-      <div className={` ${footerStyles.footer__contentRight}`}>
+      <div className={` ${footerStyles.footer__contentRight} `}>
         <h3>Space for social media icons.  </h3>
-        <a href="" ><img src={facebook} alt="Facebook Icon" /></a>
-        <a href="https://instagram.com/growbyspringload" ><img src={instagram} alt="Instagram Icon" /></a>
-        <a href="https://twitter.com/growspringload" ><img src={twitter} alt="Twitter Icon" /></a>
+        <a className={`${footerStyles.footer__img}`} href="" ><img src={facebook} alt="Facebook Icon" /></a>
+        <a className={`${footerStyles.footer__img}`} href="https://instagram.com/growbyspringload" ><img src={instagram} alt="Instagram Icon" /></a>
+        <a className={`${footerStyles.footer__img}`} href="https://twitter.com/growspringload" ><img src={twitter} alt="Twitter Icon" /></a>
+        <a className={`${footerStyles.footer__img}`} href="" ><img src={github} alt="GitHub Icon" /></a>
         <p>
           {" "}
           What? We're not at all alike!
