3d78e999e876b0504fde96ae1fe92055df644f26
Zoe <zoe.richards@springload.co.nz>
2018-12-18 16:46:39 +1300

    replace header code with new version

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,6 @@
 import React from "react";
-import headerStyles from "./header-css-modules.scss";
+import headerStyles from "./header.module.scss";
+import icon from "../../static/springload_icon.svg";
 
 export default () => (
   <nav className={`${headerStyles.header__navigation}`}>
@@ -37,116 +38,7 @@ export default () => (
         <a href="http://www.cs.virginia.edu/~robins/YouAndYourResearch.html">
           Join
         </a>
-<<<<<<< HEAD
-      </div>
-      <div className={headerStyles.logo} />
-      <ul
-        className={`${headerStyles.header__list} ${
-          headerStyles.flex__container
-        } ${headerStyles.flexEnd}`}
-      >
->>>>>>> b864015... add header styling
-        <li>
-          <a href="http://www.cs.virginia.edu/~robins/YouAndYourResearch.html">
-            Springload
-          </a>
-        </li>
-      </ul>
-      <div className="springload-logo" />
-      <ul className="nav flexContainer flexEnd">
-        <li>
-          <a href="http://www.cs.virginia.edu/~robins/YouAndYourResearch.html">
-            Home
-          </a>
-        </li>
-        <li>
-          <a href="/about-css-modules">About</a>
-        </li>
-        <li>
-          <a href="http://www.cs.virginia.edu/~robins/YouAndYourResearch.html">
-            Contact
-          </a>
-        </li>
-      </ul>
-    </nav>
-  </div>
-=======
       </li>
     </ul>
   </nav>
-<<<<<<< HEAD
-  // </div>
-
-  // <div className={headerStyles.bluebackground} >
-  // <div className={`${headerStyles.header__navigation}`}>
-  //     <div className={`${headerStyles.header__item}  ${headerStyles.flex__item} `}>
-  //       <span className={`${headerStyles.header__grow}`}>GROW </span>
-  //       <a
-  //         className={`${headerStyles.header__item}`}
-  //         href="http://www.cs.virginia.edu/~robins/YouAndYourResearch.html"
-  //       >
-  //         Springload
-  //       </a>
-  //     </div>
-  //     <div className={headerStyles.logo} />
-  //     <ul
-  //       className={`${headerStyles.header__list} ${headerStyles.flex__container} ${
-  //         headerStyles.flexEnd
-  //       }`}
-  //     >
-  //       <li>
-  //         <a href="http://www.cs.virginia.edu/~robins/YouAndYourResearch.html">
-  //           Home
-  //         </a>
-  //       </li>
-  //       <li>
-  //         <a href="/about-css-modules">About</a>
-  //       </li>
-  //       <li>
-  //         <a href="http://www.cs.virginia.edu/~robins/YouAndYourResearch.html">
-  //           Contact
-  //         </a>
-  //       </li>
-  //       <li>
-  //         <a href="http://www.cs.virginia.edu/~robins/YouAndYourResearch.html">
-  //           Join
-  //         </a>
-  //       </li>
-  //     </ul>
-  //   </div>
-
-  // <div className={headerStyles.container}>
-  //   {children}
-  //   <nav className="home__navigation">
-  //     <ul className="nav flexItem flexStart">
-  //       <li>
-  //         <img src={icon} alt="icon" />
-  //
-  //         <span className="grow">GROW </span>
-  //         <a href="http://www.cs.virginia.edu/~robins/YouAndYourResearch.html">
-  //           Springload
-  //         </a>
-  //       </li>
-  //     </ul>
-  //     <div className="springload-logo" />
-  //     <ul className="nav flexContainer flexEnd">
-  //       <li>
-  //         <a href="http://www.cs.virginia.edu/~robins/YouAndYourResearch.html">
-  //           Home
-  //         </a>
-  //       </li>
-  //       <li>
-  //         <a href="/about-css-modules">About</a>
-  //       </li>
-  //       <li>
-  //         <a href="http://www.cs.virginia.edu/~robins/YouAndYourResearch.html">
-  //           Contact
-  //         </a>
-  //       </li>
-  //     </ul>
-  //   </nav>
-  // </div>
->>>>>>> 453fe8c... add flex styling
-=======
->>>>>>> 10eef2d... fix header styling
 );
