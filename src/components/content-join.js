9a2346ddc49b39d5f6da035f75ed8c5d3b98984a
Zoe <zoe.richards@springload.co.nz>
2018-12-20 12:02:29 +1300

    add new pages for join page

---
+++ src/components/content-join.js
@@ -0,0 +1,10 @@
+import React from "react";
+import contentStyles from "./content-join.module.scss";
+
+export default () => (
+  <div
+    className={`${contentStyles.flex__container} ${contentStyles.flex__item}`}
+  >
+    <p>Insert lilregie form here</p>
+  </div>
+);
---
+++ src/pages/join-css-modules.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "./about-css-modules.module.scss";
+import Container from "../components/container";
+import Header from "../components/header";
+import Content from "../components/content-join";
+import Footer from "../components/footer";
+
+console.log(styles);
+
+export default () => (
+  <div>
+    <Header springLoad="Springload" about="about" home="home" join="join" />
+    <div
+      className={`${styles.flex__container} ${styles.banner}
+	      ${styles.flexCenter} ${styles.itemCenter} ${styles.imageHeight} ${
+        styles.centerImage
+      }`}
+    >
+      <aside className={`${styles.banner__overlay}`}>
+        <h2>JOIN GROW!</h2>
+      </aside>
+    </div>
+    <Container>
+      <Content />
+    </Container>
+    <Footer footerDescription="Springload" />
+  </div>
+);
