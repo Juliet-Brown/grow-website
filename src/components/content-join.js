946cae788581275cce86135abf3a006e535eda1d
Zoe <zoe.richards@springload.co.nz>
2018-12-21 09:56:49 +1300

    change bem styling

--- src/components/content-join.js
+++ src/components/content-join.js
@@ -5,7 +5,7 @@ export default () => (
   <div
     className={`${contentStyles.flex__container} ${contentStyles.flex__item}`}
   >
-    <div className={` ${contentStyles.form}`}>
+    <div className={` ${contentStyles.content}`}>
       <h2>Insert lilregie form here</h2>
     </div>
   </div>
